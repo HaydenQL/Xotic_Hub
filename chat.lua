9433ef997304985bd4c690f5feb92bb7b60acf0c
Xeno1234112 <Haydenmarkins1@gmail.com>
2025-03-03 19:32:36 -0600

    chat.lua

--- chat.lua
+++ chat.lua
@@ -1,3 +1,10 @@
-local player = game.Players.LocalPlayer
-task.wait(1) -- Waits a second before kicking
-player:Kick("Disconnected.")
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+local old = mt.__namecall
+
+mt.__namecall = newcclosure(function(self, ...)
+    if tostring(getnamecallmethod()) == "Kick" and self == game.Players.LocalPlayer then
+        return old(self, "You have been kicked.")
+    end
+    return old(self, ...)
+end)
