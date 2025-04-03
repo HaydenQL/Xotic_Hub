12aa6135e9e6e5947c1364cb68d8bc0e4aab7366
Xeno1234112 <Haydenmarkins1@gmail.com>
2025-04-03 03:45:12 -0500

    Update and rename give.lua to Test2.lua

--- give.lua
+++
@@ -1,7 +0,0 @@
-local remote = game:GetService("ReplicatedStorage"):WaitForChild("GiveSwordRemote")
-
--- Give to yourself
-remote:FireServer("HaydenSigma24")
-
--- Give to your friend
-remote:FireServer("noctiphobiia")
