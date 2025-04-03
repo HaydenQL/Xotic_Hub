3441a49c6ee7ff5e77d2a4b6fe1ee4943dc04015
Xeno1234112 <Haydenmarkins1@gmail.com>
2025-04-03 17:32:06 -0500

    Update Nwwaas.lua

--- Nwwaas.lua
+++ Nwwaas.lua
@@ -214,7 +214,7 @@ mainFrame.ClipsDescendants = true
 contentFrame.ClipsDescendants = true
 
 -- WalkSpeed Input
-local walkSpeedLabel = Instance.new("ScrollingFrame")
+local walkSpeedLabel = Instance.new("TextButton")
 walkSpeedLabel.Size = UDim2.new(0, 200, 0, 20)
 walkSpeedLabel.Position = UDim2.new(0, 20, 0, 50)
 walkSpeedLabel.BackgroundTransparency = 1
