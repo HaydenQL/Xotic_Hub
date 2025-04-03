5f3ee68607a6fd050f12c0c74e9c6de126bf0417
Xeno1234112 <Haydenmarkins1@gmail.com>
2025-04-03 11:46:29 -0500

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -1,10 +1,14 @@
--- Find and list all RemoteEvents in the game
-for _, v in pairs(game:GetDescendants()) do
-    if v:IsA("RemoteEvent") then
-        print("RemoteEvent found: " .. v:GetFullName()) -- This prints the full path
-    end
-end
-
-
-setclipboard(table.concat(remotes, "\n")) -- Copies the list to clipboard
-print("Copied all remotes to clipboard.")
+local remotes = {}
+ 
+ -- Find and list all RemoteEvents in the game
+ for _, v in pairs(game:GetDescendants()) do
+     if v:IsA("RemoteEvent") or v:IsA("RemoteFunction") then
+         table.insert(remotes, v:GetFullName())
+     if v:IsA("RemoteEvent") then
+         print("RemoteEvent found: " .. v:GetFullName()) -- This prints the full path
+     end
+ end
+ 
+ 
+ setclipboard(table.concat(remotes, "\n")) -- Copies the list to clipboard
+ print("Copied all remotes to clipboard.")
