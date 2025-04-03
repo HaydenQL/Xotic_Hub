3a9c5c88a4cb9da29455ac972da8e0527c4e974a
Xeno1234112 <Haydenmarkins1@gmail.com>
2025-04-03 18:08:18 -0500

    Update testtt.lua

--- testtt.lua
+++ testtt.lua
@@ -1,1 +1,5 @@
-game.ReplicatedStorage.SomePopupRemote:FireServer("Message here")
+-- Run this once when your GUI loads
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local remote = ReplicatedStorage:FindFirstChild("LineformEvent") or Instance.new("RemoteEvent")
+remote.Name = "LineformEvent"
+remote.Parent = ReplicatedStorage
