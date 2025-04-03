013a5af79c17b606f0a69dcab687b1ef4bc24497
Xeno1234112 <Haydenmarkins1@gmail.com>
2025-04-03 02:28:33 -0500

    Update give.lua

--- give.lua
+++ give.lua
@@ -1,19 +1,7 @@
--- [⚔️ Remote Sword Giver to Another Player]
-local remote = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):FindFirstChild("GiveTool")
-local target = game:GetService("Players"):FindFirstChild("itschiefkeefhoe")
+local remote = game:GetService("ReplicatedStorage"):WaitForChild("GiveSwordRemote")
 
-if remote and target then
-	local tool = Instance.new("Tool")
-	tool.Name = "XenoBlade"
-	tool.RequiresHandle = true
+-- Give to yourself
+remote:FireServer("HaydenSigma24")
 
-	local handle = Instance.new("Part")
-	handle.Name = "Handle"
-	handle.Size = Vector3.new(1, 4, 1)
-	handle.BrickColor = BrickColor.new("Really red")
-	handle.Material = Enum.Material.Neon
-	handle.CanCollide = false
-	handle.Parent = tool
-
-	remote:FireServer(tool, target)
-end
+-- Give to your friend
+remote:FireServer("noctiphobiia")
