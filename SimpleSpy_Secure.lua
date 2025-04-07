68c30f726da231ad007da19f9cc0589e646ddb3a
Hayden <Haydenmarkins1@gmail.com>
2025-04-07 07:13:09 -0500

    Update SimpleSpy_Secure.lua

--- SimpleSpy_Secure.lua
+++ SimpleSpy_Secure.lua
@@ -17,7 +17,7 @@ local Players = game:GetService("Players")
 local CoreGui = game:GetService("CoreGui")
 local Highlight =
 	loadstring(
-		game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/highlight.lua")
+		game:HttpGet("https://raw.githubusercontent.com/HaydenQL/Chat-bypass/main/Highlight_Local.lua")
 	)()
 
 ---- GENERATED (kinda sorta mostly) BY GUI to LUA ----
