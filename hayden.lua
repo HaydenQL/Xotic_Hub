19856619f035ce66d07d1f8314610297384baeab
Hayden <Haydenmarkins1@gmail.com>
2025-04-29 12:20:46 -0500

    Update and rename haydenv34.lua to hayden.lua

--- hayden.lua
+++ hayden.lua
@@ -6584,11 +6584,21 @@ end
 --Load inf when in
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
+-- gives tool when injected
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
 
+local tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "Click Teleport"
 
+tool.Activated:Connect(function()
+    local pos = mouse.Hit + Vector3.new(0, 2.5, 0)
+    local cf = CFrame.new(pos.X, pos.Y, pos.Z)
+    player.Character.HumanoidRootPart.CFrame = cf
+end)
 
+tool.Parent = player.Backpack
 
 --[[ WELCOME ]]--
 ui:CreateNotification("Welcome", "Xeno has been loaded successfully", 5, "info")
