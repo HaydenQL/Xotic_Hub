8824c3b8512266d640c6f15f53f4306099628c50
Hayden <Haydenmarkins1@gmail.com>
2025-04-29 11:32:04 -0500

    Update haydenv32.lua

--- haydenv32.lua
+++ haydenv32.lua
@@ -6582,6 +6582,22 @@ function r15Suite()
     log("Script loaded and running")
 end
 
+-- gives tool when injected
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+
+local tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "Click Teleport"
+
+tool.Activated:Connect(function()
+    local pos = mouse.Hit + Vector3.new(0, 2.5, 0)
+    local cf = CFrame.new(pos.X, pos.Y, pos.Z)
+    player.Character.HumanoidRootPart.CFrame = cf
+end)
+
+tool.Parent = player.Backpack
+
 --[[ WELCOME ]]--
 ui:CreateNotification("Welcome", "Xeno has been loaded successfully", 5, "info")
 seperate("Loaded!")
