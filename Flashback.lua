c9259ba1052a3fa005eae71155c7528d116c287e
Hayden <Haydenmarkins1@gmail.com>
2025-05-01 17:48:49 -0500

    Update Flashback.lua

--- Flashback.lua
+++ Flashback.lua
@@ -1,5 +1,5 @@
 -- flashback
-local Keybind = Enum.KeyCode.C -- Hold C to rewind
+local Keybind = Enum.KeyCode.X -- Hold C to rewind
 local RewindSpeed = 1.7 -- How fast to rewind
 local FlashbackLength = 95 -- How long history stores (seconds)
 
