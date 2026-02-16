9c5624dbe54c9d1d818ec5fff45d47185d5aa302
Hayden <Haydenmarkins1@gmail.com>
2026-02-16 17:37:48 -0600

    Rename Xotic6.lua to Xotic7.lua

--- Xotic7.lua
+++ Xotic7.lua
@@ -4566,7 +4566,7 @@ ui:AddToggle(sections.miscMap, "Void Walk", false, function(state)
         void.Anchored = true
         void.CanCollide = true
         void.Transparency = 0.8
-        void.Size = Vector3.new(20000, 16, 20000)
+        void.Size = Vector3.new(1100000000, 16, 1100000000)
         void.Position = Vector3.new(0, -8, 0)
         void.Parent = workspace
 
